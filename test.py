1f6209474f5e63bb191275667fde42b016a4d8af
Xu Kang <kangx.xu@intel.com>
2015-06-05 09:49:33 +0000

    test8

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
+import os
 import os.path
 
-print "directory:%s" % os.path.abspath(__file__) 
+print "directory:%s" % os.path.abspath(__file__)
+
+for curdir, subDirs, subFiles in os.walk(os.curdir):
+  print  subFiles,
