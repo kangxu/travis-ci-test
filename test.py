3431bf9fd048631f6b16c69e8d2784f8b594456d
Xu Kang <kangx.xu@intel.com>
2015-06-05 11:03:32 +0000

    test10

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 import os
 import os.path
+import commands
 
 print "directory:%s" % os.path.abspath(__file__)
 
-for curdir, subDirs, subFiles in os.walk(os.curdir):
-  print  subFiles,
+#for curdir, subDirs, subFiles in os.walk(os.curdir):
+ #print  subFiles,
+
+print commands.getoutput("git ls-tree -r --name-only HEAD")
