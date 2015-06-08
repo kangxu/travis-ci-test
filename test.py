4a873d46b0c42f331cdc582725051e7909f9b10d
Xu Kang <kangx.xu@intel.com>
2015-06-08 09:52:54 +0000

    test12

--- test.py
+++ test.py
@@ -1,11 +1,59 @@
 #!/usr/bin/env python
+import sys
 import os
+import re
 import os.path
 import commands
 
-print "directory:%s" % os.path.abspath(__file__)
+Repo_Root = os.path.dirname(os.path.abspath(__file__))
+ERRORS = []
 
-#for curdir, subDirs, subFiles in os.walk(os.curdir):
- #print  subFiles,
+def outputErrors():
+    for errortype, errorfile in ERRORS:
+        print "%s: %s" % (errortype, errorfile)
 
-print commands.getoutput("git ls-tree -r --name-only HEAD")
+def git(cmd):
+    output = commands.getstatusoutput(cmd)
+    if output[0] != 0:
+        raise Exception("Fail to execute cmd:%s" % cmd)
+    return output[-1]
+
+def filterUpdatedFiles(gitoutput):
+    updatedFiles = []
+    for line in gitoutput.split("\n"):
+        line = line.strip()
+        status = line.split("\t")
+        if status[0].strip() != "D":
+          updatedFiles.append(status[-1].strip())
+    return updatedFiles
+
+def checkTailSpace(filepath):
+    with open(filepath) as f:
+        for line in f:
+            if not re.compile(" $").search(line):
+                bname = os.path.basename(filepath)
+                error = ["TAILING WHILTESPACE", bname]
+                ERRORS.append(error)
+                return
+
+def checkFileNameSpace(filepath):
+    bname = os.path.basename(filepath)
+    if not re.compile(" ").search(bname):
+        error = ["FILENAME WHILTESPACE", bname]
+        ERRORS.append(error)
+        return
+
+def main():
+    lints = [checkTailSpace, checkFileNameSpace]
+    gitOutput = git("git diff --name-status HEAD~1")
+    for path in filterUpdatedFiles(gitOutput):
+        filePath = os.path.join(Repo_Root, path)
+        if os.path.isfile(filePath):
+            for lint in lints:
+                lint(filePath)
+
+if __name__ == "__main__":
+    main()
+    if len(ERRORS) != 0:
+        outputErrors()
+        sys.exit(1)
